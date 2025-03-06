62a1749f7c076f47a04ea15a7bfc615c580b6f38
Carlos <156889987+msd-pan@users.noreply.github.com>
2025-03-06 13:31:59 +0900

    Adding an event listener

--- shared.js
+++ shared.js
@@ -1,5 +1,12 @@
 const backDrop = document.querySelector(".backdrop");
+const modal = document.querySelector(".modal");
+const selectPlanButton = document.querySelectorAll(".plan button");
 
 // console.log(backDrop);
 
-backDrop.style.display = "block";
+selectPlanButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    backDrop.style.display = "block";
+    modal.style.display = "block";
+  });
+});
